dc315af63938306c7b3d36e0665811ba2dcab13d
Eli <elize.san10@gmail.com>
2021-01-22 11:37:51 -0600

    reach MVP with each character (rough draft) - no styling added yet - axios call working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import { API_KEY, BASE_URL } from "./constants";
+import Characters from "./components/Character";
 
 const App = () => {
+  const [characters, setCharacters] = useState(null);
+  // const [currentCharacterId, setCurrentCharacterId] = useState(null);
+
+  // const open = (id) => {
+  //   setCurrentCharacterId(id);
+  // };
+  // const close = () => {
+  //   setCurrentCharacterId(null);
+  // };
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/people?api_key=${API_KEY}`)
+      .then((res) => {
+        setCharacters(res.data.results);
+      })
+      .catch((err) =>
+        console.log(
+          err,
+          "Something is wrong with your Star Wars api. Check axios call."
+        )
+      );
+  }, []);
+
+  if (!characters) return null;
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+
+      {characters.map((character) => {
+        return <Characters key={character.id} characters={character} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,43 @@
 // Write your Character component here
+// import axios from "axios";
+import React from "react";
+// import styled from "styled-components";
+// import { API_KEY, BASE_URL } from "../constants";
+
+const Character = ({ characters, open, close }) => {
+  //   const [bio, setBio] = useState(null);
+
+  //   useEffect(() => {
+  //     axios
+  //       .get(`${BASE_URL}/person/${characterId}?api_key=${API_KEY}`)
+  //       .then((res) => {
+  //         setBio(res.data.result);
+  //       })
+  //       .catch((err) =>
+  //         console.log(
+  //           err,
+  //           "Something is wrong with getting the id. Check axios call for id."
+  //         )
+  //       );
+  //   }, [characterId]);
+
+  return (
+    <div>
+      <div className="name">
+        <h2>{characters.name}</h2>
+        {/* <button onClick={open}>Open</button> */}
+      </div>
+
+      <div className="bio">
+        <h3>{characters.name}'s Bio</h3>
+        <p>Birth Year: {characters.birth_year}</p>
+        <p>Gender: {characters.gender}</p>
+        <p>Height: {characters.height}</p>
+        <p>Mass: {characters.mass}</p>
+        {/* <button onClick={close}>Close</button> */}
+      </div>
+    </div>
+  );
+};
+
+export default Character;
